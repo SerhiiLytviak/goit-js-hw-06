5217303990b2ad40ca6dac0eba29db67b961329d
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-14 12:14:56 +0200

    Дополнена 9-я задача
    Убрал в разметке у <body> инлайн стиль, добавил текстовое значение цвета в формате rgb(r,g,b)

--- js/task-09.js
+++ js/task-09.js
@@ -4,9 +4,11 @@ function getRandomHexColor() {
 
 const buttonRef = document.querySelector("button.change-color");
 const bodyRef = document.querySelector("body");
+const spanRef = document.querySelector(".color");
 
 buttonRef.addEventListener("click", onButtonClick);
 
 function onButtonClick() {
   bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = bodyRef.style.backgroundColor;
 }
